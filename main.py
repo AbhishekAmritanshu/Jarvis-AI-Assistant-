ca92b572c002b3b3846f4dc35972cd0e63484a01
abhishek abhi <abhiabhishek413@gmail.com>
2020-12-16 10:42:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pyttsx3
+import datetime
+import wikipedia
+import SpeechRecognition as SR
+
+engine = pyttsx3.init('sapi5')
+voice = engine.getProperty('voices')
+#print voice
+#print(voices[0].id)
+engine = setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
+  
+  def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+      speak("Good morning")
+
+    elif hour>=12 and hour<18:
+      speak("Good afternoon")
+
+    else:
+        speak("Good evening")
+        speak("I am Jarvis Sir, please tell me how I may help you")
+
+  def takeCommand():
+    #take input from microphone and start giving output
+    r= SR.recognizer()
+    with SR.microphone as source:
+      print("Listening...")
+      r.pause_threshold = 1
+      audio = r.listen(source)
+
+  try:
+    print("recognizing...")
+    query = r.recognize_google(audio, language='en-in')
+    print(f"user said: {query}\n")
+  
+  except Exception as e:
+    print(e)
+
+    print("say that again please...")
+    return "None"
+  return query
+
+
+  if __name__== "__main__":
+
+    speak("Abhishek is a good boy")
+takeCommand()
+
+
